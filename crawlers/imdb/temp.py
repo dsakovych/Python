b90904f03817498f9c7abe1a9cb78aba8a3c3805
Dima Sakovich <dima.sakovich@gmail.com>
2017-02-07 18:09:28 +0200

    3

---
+++ crawlers/imdb/temp.py
@@ -0,0 +1,47 @@
+import requests
+import re
+import time
+import csv
+from bs4 import BeautifulSoup
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36'
+                         ' (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+
+
+def make_soup(url):
+    return BeautifulSoup(requests.get(url, headers=headers).content, 'lxml')
+
+
+def main():
+    url = 'http://www.imdb.com/title/tt0204946/?ref_=adv_li_tt'
+    url1 = 'http://www.imdb.com/title/tt0215941/?ref_=adv_li_tt'
+    url2 = 'http://www.imdb.com/title/tt0266802/?ref_=adv_li_tt'
+    soup = make_soup(url1)
+    try:
+        text = soup(text=re.compile('user reviews'))[0].parent.get('href')
+        print('http://www.imdb.com/' + text)
+    except IndexError:
+        print('ololo')
+
+
+def gather_reviews():
+    url = 'http://www.imdb.com//title/tt0215941/reviews?ref_=tt_urv'
+    soup_rates = make_soup(url).findAll('h2')
+    rates = []
+    for item in soup_rates:
+        title = item.next
+        rate = item.next.next.next.get('alt') if item.next.next.next.get('alt') is not None else 'nan'
+        rates.append((title, rate))
+    soup_reviews = make_soup(url).findAll('div', {'class': 'yn'})
+    #print(len(soup_reviews))
+    reviews = []
+    for item in soup_reviews:
+        reviews.append(item.previous.previous)
+        #print(item.previous.previous)
+    results = list(zip(rates, reviews))
+    print(results)
+
+
+
+if __name__ == '__main__':
+    #main()
+    gather_reviews()
